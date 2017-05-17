b3b7866a059fd9163aa3ee1139687ac42e3ed18d
Axiu <axiublog@gmail.com>
2017-05-17 17:26:47 +0800

    add click-outside directive

---
+++ transitions/popup/click-outside-directive.js
@@ -0,0 +1,33 @@
+var Vue = require('vue');
+
+// Then create your Vue.directive as a variable:
+var clickOutsideDirective = Vue.directive('click-outside', {
+    bind: function (el, binding, vNode) {
+        // Provided expression must evaluate to a function.
+        if (typeof binding.value !== 'function') {
+            var compName = vNode.context.name;
+            var warn = '[Vue-click-outside:] provided expression ' + binding.expression + ' is not a function, but has to be';
+            if (compName) {
+                warn += 'Found in component ' + compName;
+            }
+            console.warn(warn);
+        }
+        // Define Handler and cache it on the element
+        var bubble = binding.modifiers.bubble;
+        var handler = function(e) {
+            if (bubble || (!el.contains(e.target) && el !== e.target)) {
+                binding.value(e)
+            }
+        };
+        el.__vueClickOutside__ = handler;
+        // add Event Listeners
+        document.addEventListener('click', handler)
+    },
+    unbind: function (el, binding) {
+        // Remove Event Listeners
+        document.removeEventListener('click', el.__vueClickOutside__);
+        el.__vueClickOutside__ = null
+    }
+});
+
+module.exports = clickOutsideDirective;
